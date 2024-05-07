1e3f266c94e556fb7de5933eb0b636ad96437f3a
itallominatti <itallominatti@gmail.com>
2024-05-07 19:43:00 -0300

    [ feat ] main -> Criando o authReducer

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./reducers/authReducer";
 
 export const store = configureStore({
     reducer: {
-
+        auth: authReducer,
     },
 });
 
